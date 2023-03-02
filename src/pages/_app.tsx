d0a20f8a20ede2271c40d5f97a8f672cafacfe31
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2023-03-03 05:10:56 +0900

    feat: calendar area

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/style/global.css"
 import "@/style/modern-normalize.css"
 
 import { SWRConfig } from "swr"
+import Head from "next/head"
 
 import PersonalInfoProvider from "@/lib/pocketSign/PersonalInfoProvider"
 import DebugPanel from "@/lib/pocketSign/DebugPanel"
@@ -10,12 +11,17 @@ import type { AppProps } from "next/app"
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <PersonalInfoProvider>
-      <SWRConfig>
-        <Component {...pageProps} />
-        {process.env.NODE_ENV === "development" && <DebugPanel />}
-      </SWRConfig>
-    </PersonalInfoProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width, viewport-fit=cover" />
+      </Head>
+      <PersonalInfoProvider>
+        <SWRConfig>
+          <Component {...pageProps} />
+          {process.env.NODE_ENV === "development" && <DebugPanel />}
+        </SWRConfig>
+      </PersonalInfoProvider>
+    </>
   )
 }
 
