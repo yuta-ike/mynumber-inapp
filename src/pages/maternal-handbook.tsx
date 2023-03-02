d64541e33bc44a50605ea1f43736f1e60711c831
yuuumbk <hikoki.y@gmail.com>
2023-03-03 02:50:24 +0900

    add: 母子手帳ページ

--- src/pages/maternal-handbook.tsx
+++ src/pages/maternal-handbook.tsx
@@ -1,13 +1,57 @@
+import Image from "next/image"
+import { NavArrowRight } from "iconoir-react"
+
 import BottomNavBar from "@/components/layouts/BottomNavBar"
 import TopBar from "@/components/layouts/TopBar"
+import { bgBaseColor } from "@/consts/theme"
+import Municipality from "@/images/municipality.png"
 
 const MaterilHandbook: React.FC = () => {
   return (
     <>
-      <TopBar>母子手帳</TopBar>
+      <TopBar bgcolor={bgBaseColor}>
+        <h1 className="pl-2 text-[24px]">母子手帳</h1>
+      </TopBar>
       <main>
+        <div className="relative m-4 h-[140px]">
+          <Image
+            src={Municipality}
+            alt="自治体の子育て支援"
+            layout="fill"
+            objectFit="contain"
+            priority
+            placeholder="blur"
+          />
+        </div>
         <div className="m-4">
-          <p className="text-red-400">Statstincs</p>
+          <h2 className="-mb-2 text-xl">お母さんの基本情報</h2>
+          <ul className="list-none pl-0">
+            <li className="my-1 flex items-center justify-between">
+              <p>妊婦の健康状態</p>
+              <NavArrowRight />
+            </li>
+            <li className="my-1 flex items-center justify-between">
+              <p>妊娠の職業・環境</p>
+              <NavArrowRight />
+            </li>
+          </ul>
+        </div>
+        <div className="m-4">
+          <h2 className="-mb-2 text-xl">妊娠中の経過</h2>
+          <ul className="list-none pl-0">
+            <li className="my-1 flex items-center justify-between">
+              <p>妊娠健診（妊娠の経過）</p>
+              <NavArrowRight />
+            </li>
+            <li className="my-1 flex items-center justify-between">
+              <p>検査の記録</p>
+              <NavArrowRight />
+            </li>
+            <li className="my-1 flex items-center justify-between">
+              <p>妊娠中と産後の歯の状態</p>
+              <NavArrowRight />
+            </li>
+          </ul>
         </div>
       </main>
       <BottomNavBar />
