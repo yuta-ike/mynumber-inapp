a33fbe0eddd92a936ba150f53829b3f2ed61068b
yuuumbk <hikoki.y@gmail.com>
2023-03-03 00:57:00 +0900

    add: AIがつぶやく共通コンポーネント

---
+++ src/components/AIWhisper.tsx
@@ -0,0 +1,29 @@
+import { Avatar } from "@mui/material"
+import Image from "next/image"
+
+import AI from "@/images/AI.png"
+import { theme } from "@/consts/theme"
+
+export type AIWhisperProps = {
+  children: React.ReactNode
+}
+
+const AIWhisper: React.FC<AIWhisperProps> = ({ children }) => {
+  return (
+    <div
+      className="flex items-center justify-start rounded-md bg-white p-5"
+      style={{
+        color: theme.palette.primary["500"],
+      }}
+    >
+      <div className="w-[17%]">
+        <Avatar>
+          <Image src={AI} alt="AI" />
+        </Avatar>
+      </div>
+      <p>{children}</p>
+    </div>
+  )
+}
+
+export default AIWhisper
