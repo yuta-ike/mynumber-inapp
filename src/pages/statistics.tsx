5d6dae1a79cfacf30c2e59621d002ad5b986e854
yuuumbk <hikoki.y@gmail.com>
2023-03-02 21:12:44 +0900

    add: ボトムメニューから直接アクセスできるページの雛形を作成

---
+++ src/pages/statistics.tsx
@@ -0,0 +1,16 @@
+import BottomNavBar from "@/components/layouts/BottomNavBar"
+import TopBar from "@/components/layouts/TopBar"
+
+const Statistics: React.FC = () => {
+  return (
+    <>
+      <TopBar>記録</TopBar>
+      <main>
+        <div className="m-4"></div>
+      </main>
+      <BottomNavBar />
+    </>
+  )
+}
+
+export default Statistics
